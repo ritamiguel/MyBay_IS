c673c09abd029c80f33289fb23639531ab6a5143
ritamiguel <ritadababs@hotmail.com>
2019-10-15 15:24:47 +0100

    First Commit

---
+++ projeto2-data/src/main/java/data/Item.java
@@ -0,0 +1,80 @@
+package data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String countryOrigin;
+    private String photograph;
+    private String category;
+    private double price;
+    private Date publhishedDate;
+
+    private List<Item> selledItems;
+
+
+    public Item(String name, String countryOrigin, String photograph, String category, double price, Date publhishedDate) {
+        this.name = name;
+        this.countryOrigin = countryOrigin;
+        this.photograph = photograph;
+        this.category = category;
+        this.price = price;
+        this.publhishedDate = publhishedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryOrigin() {
+        return countryOrigin;
+    }
+
+    public void setCountryOrigin(String countryOrigin) {
+        this.countryOrigin = countryOrigin;
+    }
+
+    public String getPhotograph() {
+        return photograph;
+    }
+
+    public void setPhotograph(String photograph) {
+        this.photograph = photograph;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getPublhishedDate() {
+        return publhishedDate;
+    }
+
+    public void setPublhishedDate(Date publhishedDate) {
+        this.publhishedDate = publhishedDate;
+    }
+}
---
+++ projeto2-data/src/main/java/data/User.java
@@ -0,0 +1,77 @@
+package data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String finalName;
+    private String lstName;
+    private String country;
+    private String email;
+    private String password;
+
+    private List<Item> boughtItems;
+
+    public User(long id, String finalName, String lstName, String country, String email, String password) {
+        this.id = id;
+        this.finalName = finalName;
+        this.lstName = lstName;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFinalName() {
+        return finalName;
+    }
+
+    public void setFinalName(String finalName) {
+        this.finalName = finalName;
+    }
+
+    public String getLstName() {
+        return lstName;
+    }
+
+    public void setLstName(String lstName) {
+        this.lstName = lstName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
